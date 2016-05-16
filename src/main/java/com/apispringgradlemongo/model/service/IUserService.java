e2bbf1a0eb573973613c4b1529a62b5d6d962941
Eloi Bilek <eloibilek@gmail.com>
2016-05-16 18:41:54 -0300

    Removed not used methods

--- src/main/java/com/apispringgradlemongo/model/service/IUserService.java
+++ src/main/java/com/apispringgradlemongo/model/service/IUserService.java
@@ -1,16 +1,9 @@
 package com.apispringgradlemongo.model.service;
 
-import java.util.List;
-
 import com.apispringgradlemongo.model.entity.User;
 
 /**
  * @author eloi eloibilek@gmail.com
  */
 public interface IUserService extends IOperations<User> {
-
-	List<User> findByName(String name);
-
-	List<User> findByEmail(String email);
-
 }
