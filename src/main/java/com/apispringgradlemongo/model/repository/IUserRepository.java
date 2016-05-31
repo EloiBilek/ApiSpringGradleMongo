dd0f36a0c7fd5915e8677ba800e28fd8c6bafee3
Eloi Bilek <eloibilek@gmail.com>
2016-05-30 22:48:34 -0300

    Update repository file

--- src/main/java/com/apispringgradlemongo/model/repository/IUserRepository.java
+++ src/main/java/com/apispringgradlemongo/model/repository/IUserRepository.java
@@ -1,7 +1,5 @@
 package com.apispringgradlemongo.model.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.apispringgradlemongo.model.entity.User;
@@ -10,9 +8,4 @@ import com.apispringgradlemongo.model.entity.User;
  * @author eloi eloibilek@gmail.com
  */
 public interface IUserRepository extends MongoRepository<User, String> {
-
-	List<User> findByName(String name);
-
-	List<User> findByEmail(String email);
-
 }
